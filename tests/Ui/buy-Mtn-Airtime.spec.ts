91fe69c3a73647fd0ee8fab47535e5ff324dbe6c
Abdoelmaaliek <samaaiab962@gmail.com>
2026-08-21 14:31:28 +0200

    Updated all network

---
+++ tests/Ui/buy-Mtn-Airtime.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("http://localhost:4173/");
+  await page.getByRole("link", { name: "Sign In" }).nth(1).click();
+  await page
+    .getByRole("textbox", { name: "Email address" })
+    .fill("merchant@flashgateway.local");
+
+  await page.getByRole("textbox", { name: "Password" }).fill("Password123!");
+  await page.getByRole("button", { name: "Sign In" }).click();
+  await expect(page).toHaveURL("http://localhost:4173/dashboard");
+  await page.getByRole("link", { name: "phone_iphone Buy Airtime" }).click();
+  await page.getByRole("button", { name: "M Mtn" }).click();
+  await page.getByRole("textbox", { name: "000 0000" }).fill("658525685");
+  await page.getByRole("spinbutton", { name: "0.00" }).fill("10");
+  await page.getByRole("button", { name: "Review Payment arrow_forward" }).click();
+  await page.getByRole("button", { name: "Confirm Payment" }).click();
+  await expect( page.getByText('Airtime top-up for Mtn')).toBeVisible({ timeout: 15000 });
+
+})

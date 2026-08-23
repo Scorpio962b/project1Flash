dcfce13ba4a62224e0e1300ccf9ce48a9780dd90
DESKTOP-GILNUJR\USER-PC <samaaiab962@gmail.com>
2026-08-23 23:21:28 +0200

    login working

--- tests/Ui/valid-login-test.spec.ts
+++ tests/Ui/valid-login-test.spec.ts
@@ -2,14 +2,14 @@ import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
   await page.goto('http://localhost:4173/');
-  await page.getByRole('link', { name: 'FlashGateway' }).click();
-  await page.getByRole('link', { name: 'Sign In' }).click();
+  await page.getByRole('link', { name: 'FlashGateway' });
+  await page.getByRole('link', { name: 'Sign In' }).nth(2).click();
 
 // valid credentials
   await page.getByRole('textbox', { name: 'Email address' }).fill('merchant@flashgateway.local');
   await page.getByRole('textbox', { name: 'Password' }).fill('Password123!');  
 
   await page.getByRole('button', { name: 'Sign In' }).click();
-  await expect(page.getByRole('heading', { name: 'Portfolio Overview' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Portfolio Overview' })).toBeVisible({timeout :150000});
   
 });
